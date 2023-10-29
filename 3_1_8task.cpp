71ac0d84e243be3cedb673f1167bb735821d9f41
mgwr <aaskarov870@gmail.com>
2023-10-29 14:40:24 +0500

    modified:   3_1_8task.cpp

--- 3_1_8task.cpp
+++ 3_1_8task.cpp
@@ -7,15 +7,16 @@ void get_cur_dir(char *dst);
 int create(unsigned long long int disk_size);
 int isCreated(fileNode *f);
 int create_dir(const char* path);
-int fileNodeCreateDir(const char* path, int fnSize, short int isDir);
+int fileNodeCreateObj(const char* path, int fnSize, short int isDir);
+int fileNodeCreateDir(const char* path);
 
 typedef struct fileNode
 {
     unsigned long long int disk_size;
+    unsigned long long int size;
     char* name;
     short int isDir;
-    unsigned long long int heirsCount;
-
+    int heirsCount;
     struct fileNode *parent; 
     struct fileNode *heirs; //subdirs
     char* absolute_path;
@@ -25,13 +26,20 @@ typedef struct fileNode
 
 fileNode *FM = NULL, *CUR_DIR = NULL;
 
-int fileNodeCreateDir(const char* path, int fnSize, short int isDir){
-    if(path[0] == '/'){
-         
-    }
+int fileNodeCreateDir(char* path){
+    ++FM->heirsCount;
+    FM->heirs = (fileNode*)realloc(FM->heirs, FM->heirsCount);
+    FM->heirs[FM->heirsCount - 1].name = (char*)malloc(sizeof(char) * strlen(path));
+    FM->heirs[FM->heirsCount - 1].parent = FM;
+    FM->heirs[FM->heirsCount - 1].isDir = 1;
+    FM->heirs[FM->heirsCount - 1].size = 0;
+    FM->heirs[FM->heirsCount - 1].absolute_path = NULL;
+    FM->heirs[FM->heirsCount - 1].heirs = NULL;
+    FM->heirs[FM->heirsCount - 1].heirsCount = 0;
+    return 1;
 }
 
-int create_dir(const char* path){ return isCreated(FM) == 0 ? 0 : fileNodeCreateDir(path, 0, 1); }
+int create_dir(const char* path){ return isCreated(FM) == 0 ? 0 : fileNodeCreateDir(path); }
 
 void get_cur_dir(char *dst){ strcpy(dst, CUR_DIR->absolute_path); }
 
@@ -41,15 +49,12 @@ int create(unsigned long long int disk_size){
     else{
         FM = (fileNode*)malloc(sizeof(fileNode));
         if(FM == NULL) return 0;
-        // FM->name = (char*)malloc(sizeof(char));
-        // FM->absolute_path = (char*)malloc(sizeof(char));
-        // FM->name[0] = '/';
-        // FM->absolute_path[0] = '/';
         FM->parent = NULL;
         FM->heirs = NULL;
         FM->heirsCount = 0;
-
+        FM->size = 0;
         FM->disk_size = disk_size;
+        create_dir("/");
         fprintf(stdout, "success: file manager created with disk size %d bytes", FM->disk_size);
         return 1;
     }
