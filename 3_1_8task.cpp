f34835d68e166eccfaceafaf013268c6b01fe2ca
mgwr <aaskarov870@gmail.com>
2023-10-30 23:17:17 +0500

    modified:   3_1_8task.cpp

--- 3_1_8task.cpp
+++ 3_1_8task.cpp
@@ -26,14 +26,15 @@ int create(unsigned long long int disk_size);
 
 int chkDsk(fileNode *fn);
 int fileNodeMkObj(char *path, int mode, int file_size);
-int fileNodeChDir(char *path);
+int fileNodeChDirGlobal(char *path);
+int fileNodeChDirLocal(char *path);
 void fileNodeGoToRoot();
 
 int create_dir(const char* path){ return chkDsk(FM) == 0 ? 0 : fileNodeMkObj((char*)path,1,0); }
 int create_file(const char* path, int file_size) { return chkDsk(FM) == 0 ? 0 : fileNodeMkObj((char*)path, 0, file_size); }
-int change_dir(const char* path) { return chkDsk(FM) == 0 ? 0 : fileNodeChDir((char*)path); }
+int change_dir(const char* path) { return chkDsk(FM) == 0 ? 0 : fileNodeChDirGlobal((char*)path); }
 
-int fileNodeChDir(char *path){
+int fileNodeChDirGlobal(char *path){
     char *token, *string, *toFree;
     string = strdup(path);
     if(string != NULL){
@@ -56,6 +57,10 @@ int fileNodeChDir(char *path){
     return 1;
 }
 
+int fileNodeChDirLocal(char *path){
+    
+}
+
 void fileNodeGoToRoot(){
     while(FM->parent != NULL){
         FM = FM->parent;
@@ -76,14 +81,23 @@ int fileNodeMkObj(char *path, int mode, int file_size){
         FM->heirs[FM->heirsCount - 1].parent = FM;
         FM->heirs[FM->heirsCount - 1].isDir = mode;
         FM->heirs[FM->heirsCount - 1].size = file_size;
-        FM->heirs[FM->heirsCount - 1].absolute_path = NULL;
+        // FM->heirs[FM->heirsCount - 1].absolute_path = NULL;
+
+        FM->heirs[FM->heirsCount - 1].absolute_path = (char*)malloc(sizeof(char)*(strlen(path) + strlen(FM->absolute_path)));
+        strcpy(FM->heirs[FM->heirsCount - 1].absolute_path, FM->absolute_path);
+        strcat(FM->heirs[FM->heirsCount - 1].absolute_path, path);
+
         FM->heirs[FM->heirsCount - 1].heirs = NULL;
         FM->heirs[FM->heirsCount - 1].heirsCount = 0;
         DskSz -= file_size;
-        if(mode == 1) fprintf(stdout, "success: created directory %s\n", path);
-        if(mode == 0) fprintf(stdout, "success: created file %s with size %d bytes in %s\n", path, file_size, FM->name);
+        if(mode == 1) fprintf(stdout, "success: created directory %s in %s\n", path, FM->absolute_path);
+        if(mode == 0) fprintf(stdout, "success: created file %s in %s with size %d bytes in %s\n", path, FM->absolute_path , file_size, FM->name);
 
         return 1;
+    }
+    if(path[0] == '/'){
+        fileNodeGoToRoot();
+
     }
     return 1;
 }
