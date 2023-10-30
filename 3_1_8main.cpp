c96642420fcfa125fc602f96b0c76f4631735f40
mgwr <aaskarov870@gmail.com>
2023-10-30 13:54:43 +0500

    modified:   3_1_8main.cpp 	modified:   3_1_8task.cpp

--- 3_1_8main.cpp
+++ 3_1_8main.cpp
@@ -5,22 +5,34 @@
 
 int main()
 {
+
+        file_manager_t fm;
+        setup_file_manager(&fm);
+        fm.create(42069);
+        fm.create_dir("cat");
+        fm.create_dir("cat/says");
+        fm.create_dir("cat/says/meow");
+        fm.create_dir("cat/doesnt");
+        fm.create_dir("cat/doesnt/says");
+        fm.create_dir("cat/doesnt/says/woof");
+        fm.create_dir("/dream_numbers");
+        fm.create_dir("/dream_numbers/420");
+        fm.create_dir("/henpecked_numbers");
+        fm.create_dir("/henpecked_numbers/69");
+
     // create(100);
     // create_dir("teest");
     // create_dir("teest1");
     // create_dir("teest2");
     // create_dir("teest3");
     // create_file("qweqweqwe.txt", 10);
-    int s_len = 0;
-    char **test = NULL;
-    parseStr((char*)"/qwe/123/zzz/qqqeqeqeqqqeqeqeqqqeqqqqeqeqeqqqeqeqeqqqeqqqqeqeqeqqqeqeqeqqqeqqqqeqeqeqqqeqeqeqqqeqqqqeqeqeqqqeqeqeqqqeq", &test , &s_len);
 
-    for(int i = 0; i < s_len; ++i) {
-        printf("%s\n", test[i]);
-    }
+    // int s_len = 0;
+    // char **test = NULL;
+    // parseStr((char*)"/qwe/123/zzz/qsq/qwedqweqwe/sdferfjknevjkner/qlkjefe", &test , &s_len);
 
-
-    // fprintf(stdout, "%s", test[0]);
-    // create(10000);
+    // for(int i = 0; i < s_len; ++i) {
+    //     printf("%s\n", test[i]);
+    // }
     return 0;
 }
--- 3_1_8task.cpp
+++ 3_1_8task.cpp
@@ -5,7 +5,6 @@
 
 typedef struct fileNode
 {
-    unsigned long long int dsksz;
     unsigned long long int size;
     char* name;
     short int isDir;
@@ -16,15 +15,20 @@ typedef struct fileNode
     
 } fileNode;
 
+int DskSz = -1;
+
 fileNode *FM = NULL, *CUR_DIR = NULL;
 
+void setup_file_manager(file_manager_t *fm);
+int create_dir(const char* path);
+int change_dir(const char* path);
+int create_file(const char* path, int file_size);
 void get_cur_dir(char *dst);
 int create(unsigned long long int disk_size);
 int chkDsk(fileNode *fn);
-int create_dir(const char* path);
-int create_file(const char* path, int file_size);
 int fileNodeMkObj(char *path, int mode, int file_size);
 void parseStr(char *path, char ***parsed_str, int *s_len);
+
 
 
 //mode: 0 - file , 1 - directory
@@ -40,19 +44,19 @@ int fileNodeMkObj(char *path, int mode, int file_size){
         FM->heirs[FM->heirsCount - 1].absolute_path = NULL;
         FM->heirs[FM->heirsCount - 1].heirs = NULL;
         FM->heirs[FM->heirsCount - 1].heirsCount = 0;
-        FM->dsksz -= file_size;
+        DskSz -= file_size;
         if(mode == 1) fprintf(stdout, "success: created directory %s\n", path);
         if(mode == 0) fprintf(stdout, "success: created file %s with size %d bytes in %s\n", path, file_size, FM->name);
         return 1;
     }
     return 1;
-
 }
 
 int create_dir(const char* path){ return chkDsk(FM) == 0 ? 0 : fileNodeMkObj((char*)path,1,0); }
 int create_file(const char* path, int file_size) { return chkDsk(FM) == 0 ? 0 : fileNodeMkObj((char*)path, 0, file_size); }
+// int change_dir(const char* path) { return chkDsk(FM) == 0 ? 0 : }
 
-int create(unsigned long long int disk_size){
+int create(int disk_size){
     if(chkDsk(FM)){ fprintf(stdout, "error: file manager already exists\n"); return 0; }
     else if(disk_size <= 0){ fprintf(stdout, "error: wrong disk size\n"); return 0; }
     else{
@@ -62,13 +66,13 @@ int create(unsigned long long int disk_size){
         FM->heirs = NULL;
         FM->heirsCount = 0;
         FM->size = 0;
-        FM->dsksz = disk_size;
         FM->absolute_path = (char*)malloc(sizeof(char));
         FM->name = (char*)malloc(sizeof(char));
         FM->absolute_path[0] = '/';
         FM->name[0] = '/';
         FM->isDir = 1;
-        fprintf(stdout, "success: file manager created with disk size %d bytes\n", FM->dsksz);
+        DskSz = disk_size;
+        fprintf(stdout, "success: file manager created with disk size %d bytes\n", DskSz);
         fprintf(stdout, "root created successfully\n");
         return 1;
     }
@@ -90,8 +94,19 @@ void parseStr(char *path, char ***parsed_str, int *s_len){
             // strncpy((*parsed_str)[(*s_len) - 1], token, strlen(token));
             (*parsed_str)[(*s_len) - 1] = strdup(token);
         }
-        free(toFree);
+        free(toFree); 
     }
+}
+
+void setup_file_manager(file_manager_t *fm) {
+    fm->create = create;
+    fm->create_dir = create_dir;
+    fm->create_file = create_file;
+    // fm->destroy = fm_destroy;
+    // fm->get_cur_dir = fm_get_cur_dir;
+    // fm->remove = fm_remove;
+    // fm->change_dir = fm_change_dir;
+    // fm->move = fm_move;
 }
 
 int chkDsk(fileNode *fn){ return fn == NULL ? 0 : 1; }
