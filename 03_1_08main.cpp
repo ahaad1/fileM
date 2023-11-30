3abd56aab2e36855a3363f62b80aeeb41232f37c
Ahad Askarov <askarov.ahad@tbcacademy.edu.ge>
2023-11-30 18:20:29 +0500

    modified:   03_1_08main.cpp 	modified:   03_1_08task.cpp 	new file:   old03_1_08task.cpp

--- 03_1_08main.cpp
+++ 03_1_08main.cpp
@@ -8,116 +8,105 @@ int main()
 {
     file_manager_t fm;
     setup_file_manager(&fm);
+    fm.create(1414);
+    fm.create_dir("/a");
+    printTree(__cwd);
+    // // // Common file manager test 1
+    // printf("%d\t", fm.create(1000)); /* expected: 1, passed */
+    // printf("%d\t", fm.create_file("test_file",1000+1)); /* expected: 0, passed */
+    // printf("%d\t", fm.destroy()); /* expected: 1, passed */
+    // printf("%d\t", fm.destroy()); /* expected: 0, passed */
+    // printf("%d\t", fm.create(1000)); /* expected: 1, passed */
+    // printf("%d\t", fm.create(1000)); /* expected: 0, passed */
+    // printf("%d\t", fm.destroy()); /* expected: 1, passed */
+    // printf("\n\n");
+    // // // Test 1 passed
+    // // // Common file manager test 2
+    // printf("%d\t", fm.create(1000)); /* expected: 1, passed */
+    // printf("%d\t", fm.create_file("file1.txt",1000)); /* expected: 1, passed */
+    // printf("%d\t", fm.create_file("file2.txt",1)); /* expected: 0, passed */
+    // printf("%d\t", fm.remove("file1.txt",0)); /* expected: 1, passed */
+    // // fm.change_dir("/");
+    // // printTree(__ind);
+    // // printf("\n=================\n");
+    // printf("%d\t", fm.remove("file1.txt",0)); /* expected: 0, passed */
+    // printf("%d\t", fm.create_file("file2.txt",1000/2)); /* expected: 1, passed */
+    // printf("%d\t", fm.create_file("file2.txt",1000/2)); /* expected: 0, passed */
+    // printf("%d\t", fm.create_file("file3.txt",1000/2)); /* expected: 1, passed */
+    // printf("%d\t", fm.create_dir("test_dir")); /* expected: 1, passed */
+    // printf("%d\t", fm.remove("file2.txt",0)); /* expected: 1, passed */
+    // printf("%d\t", fm.destroy()); /* expected: 1, passed */
+    // printf("\n\n");
+    // // // Test 2 passed
+    // // // Common file manager test 3
+    // printf("%d\t",  fm.create(1000)); /* expected: 1, passed */
+    // printf("%d\t",  fm.create_dir("dir1")); /* expected: 1, passed */
+    // printf("%d\t",  fm.create_dir("dir1/dir11")); /* expected: 1, passed */
+    // printf("%d\t",  fm.change_dir("dir1")); /* expected: 1, passed */
+    // printf("%d\t",  fm.create_dir("../dir2")); /* expected: 1, passed */
+    // printf("%d\t",  fm.create_dir("../dir2/dir3")); /* expected: 1, passed */
+    // printf("%d\t", fm.remove("/dir2/dir3", 0)); /* expected: 1, passed */
+    // printf("%d\t", fm.create_dir("/dir3/dir31")); /* expected: 0, */     
+    // // // Test 3 failed
 
-    // // Common file manager test 1
-    printf("%d\t", fm.create(1000)); /* expected: 1, passed */
-    printf("%d\t", fm.create_file("test_file",1000+1)); /* expected: 0, passed */
-    printf("%d\t", fm.destroy()); /* expected: 1, passed */
-    printf("%d\t", fm.destroy()); /* expected: 0, passed */
-    printf("%d\t", fm.create(1000)); /* expected: 1, passed */
-    printf("%d\t", fm.create(1000)); /* expected: 0, passed */
-    printf("%d\t", fm.destroy()); /* expected: 1, passed */
-    printf("\n\n");
-    // // Test 1 passed
-
-    // // Common file manager test 2
-    printf("%d\t", fm.create(1000)); /* expected: 1, passed */
-    printf("%d\t", fm.create_file("file1.txt",1000)); /* expected: 1, passed */
-    printf("%d\t", fm.create_file("file2.txt",1)); /* expected: 0, passed */
-    printf("%d\t", fm.remove("file1.txt",0)); /* expected: 1, passed */
-    
+    // fm.create(2023);
+    // fm.create_dir("/a");
+    // fm.create_dir("/a");
+    // fm.create_dir("/a");
+    // fm.create_dir("/a/b");
+    // fm.create_file("/a/a.txt", 10);
+    // fm.change_dir("/a/");
+    // fm.change_dir("/a/a.txt");
+    // fm.create_dir("/a/b/c");
+    // fm.create_file("/a/qwe.txt", 100);
+    // fm.change_dir("/a");
+    // fm.change_dir("/a/b/c/a");
+    // fm.create(100);
+    // fm.create_dir("/Desktop");
+    // fm.create_dir("/Documents");
+    // fm.create_dir("/Downloads");
+    // fm.create_dir("/file_Manager");
+    // fm.change_dir("/Desktop");
+    // fm.create_dir("./lesson");
+    // fm.create_dir("./python_Course_Tinkoff");
+    // fm.change_dir("./lesson");
+    // fm.create_dir("./a.out");
+    // fm.create_file("./a.out", 1);
+    // fm.create_file("./FCFS", 1);
+    // fm.create_file("./input.txt", 1);
+    // fm.change_dir("../python_Course_Tinkoff");
+    // fm.create_file("./main.py", 1);
+    // fm.create_file("./taxis", 1);
+    // fm.create_file("./test.ipynb", 1);
+    // fm.change_dir("../../Downloads");
+    // fm.create_dir("/Telegram Desktop");
+    // fm.change_dir("./Telegram Desktop");
+    // fm.create_file("./Compiler Explorer code.cpp", 1);
+    // fm.create_file("./FileName(2).cpp", 1);
+    // fm.create_file("./makefile", 1);
+    // fm.create_file("./New_Sort", 1);
+    // fm.change_dir("..");
+    // fm.create_file("main.cpp", 1);
+    // fm.create_file("os_file.h", 1);
+    // fm.change_dir("../file_Manager");
+    // fm.create_file("./22_1_2task", 1);
+    // fm.create_file("./22_1_2task.c", 1);
+    // fm.create_file("./os_file", 1);
     // fm.change_dir("/");
-    // printTree(__ind);
-    // printf("\n=================\n");
-    
-    printf("%d\t", fm.remove("file1.txt",0)); /* expected: 0, passed */
-    printf("%d\t", fm.create_file("file2.txt",1000/2)); /* expected: 1, passed */
-    printf("%d\t", fm.create_file("file2.txt",1000/2)); /* expected: 0, passed */
-    printf("%d\t", fm.create_file("file3.txt",1000/2)); /* expected: 1, passed */
-    printf("%d\t", fm.create_dir("test_dir")); /* expected: 1, passed */
-    printf("%d\t", fm.remove("file2.txt",0)); /* expected: 1, passed */
-    printf("%d\t", fm.destroy()); /* expected: 1, passed */
-    printf("\n\n");
-    // // Test 2 passed
-
-    // // Common file manager test 3
-    printf("%d\t",  fm.create(1000)); /* expected: 1, passed */
-    printf("%d\t",  fm.create_dir("dir1")); /* expected: 1, passed */
-    printf("%d\t",  fm.create_dir("dir1/dir11")); /* expected: 1, passed */
-    printf("%d\t",  fm.change_dir("dir1")); /* expected: 1, passed */
-    printf("%d\t",  fm.create_dir("../dir2")); /* expected: 1, passed */
-    printf("%d\t",  fm.create_dir("../dir2/dir3")); /* expected: 1, passed */
-    printf("%d\t", fm.remove("/dir2/dir3", 0)); /* expected: 1, passed */
-    printf("%d\t", fm.create_dir("/dir3/dir31")); /* expected: 0, */     
-
-    fm.destroy();
-
-    // printf("\n===test3===\n");
+    // fm.create_file("file1.txt",1000);
+    // fm.remove("file1.txt",0);
+    // fm.remove("file1.txt",0);
     // fm.change_dir("/");
-    // printTree(__ind);
-    // printf("\n=================\n");
-    
-    // // Test 3 failed
-
-    fm.create(2023);
-    fm.create_dir("/a");
-    fm.create_dir("/a");
-    fm.create_dir("/a");
-    fm.create_dir("/a/b");
-    fm.create_file("/a/a.txt", 10);
-    fm.change_dir("/a/");
-    fm.change_dir("/a/a.txt");
-    fm.create_dir("/a/b/c");
-    fm.create_file("/a/qwe.txt", 100);
-    fm.change_dir("/a");
-    fm.change_dir("/a/b/c/a");
-    fm.create(100);
-    fm.create_dir("/Desktop");
-    fm.create_dir("/Documents");
-    fm.create_dir("/Downloads");
-    fm.create_dir("/file_Manager");
-    fm.change_dir("/Desktop");
-    fm.create_dir("./lesson");
-    fm.create_dir("./python_Course_Tinkoff");
-    fm.change_dir("./lesson");
-    fm.create_dir("./a.out");
-    fm.create_file("./a.out", 1);
-    fm.create_file("./FCFS", 1);
-    fm.create_file("./input.txt", 1);
-    fm.change_dir("../python_Course_Tinkoff");
-    fm.create_file("./main.py", 1);
-    fm.create_file("./taxis", 1);
-    fm.create_file("./test.ipynb", 1);
-    fm.change_dir("../../Downloads");
-    fm.create_dir("/Telegram Desktop");
-    fm.change_dir("./Telegram Desktop");
-    fm.create_file("./Compiler Explorer code.cpp", 1);
-    fm.create_file("./FileName(2).cpp", 1);
-    fm.create_file("./makefile", 1);
-    fm.create_file("./New_Sort", 1);
-    fm.change_dir("..");
-    fm.create_file("main.cpp", 1);
-    fm.create_file("os_file.h", 1);
-    fm.change_dir("../file_Manager");
-    fm.create_file("./22_1_2task", 1);
-    fm.create_file("./22_1_2task.c", 1);
-    fm.create_file("./os_file", 1);
-    fm.change_dir("/");
-    fm.create_file("file1.txt",1000);
-    fm.remove("file1.txt",0);
-    fm.remove("file1.txt",0);
-    fm.change_dir("/");
-    fm.create_dir("cat");
-    fm.create_dir("cat/says");
-    fm.create_dir("cat/says/meow");
-    fm.create_dir("cat/doesnt");
-    fm.create_dir("cat/doesnt/says");
-    fm.create_dir("cat/doesnt/says/woof");
-    fm.create_dir("/dream_numbers");
-    fm.create_dir("/dream_numbers/420");
-    fm.create_dir("/henpecked_numbers");
-    fm.create_dir("/henpecked_numbers/69");
-    fm.destroy();
-    // // prnt(&__ind);
+    // fm.create_dir("cat");
+    // fm.create_dir("cat/says");
+    // fm.create_dir("cat/says/meow");
+    // fm.create_dir("cat/doesnt");
+    // fm.create_dir("cat/doesnt/says");
+    // fm.create_dir("cat/doesnt/says/woof");
+    // fm.create_dir("/dream_numbers");
+    // fm.create_dir("/dream_numbers/420");
+    // fm.create_dir("/henpecked_numbers");
+    // fm.create_dir("/henpecked_numbers/69");
+    // fm.destroy();
     return 0;
 }
--- 03_1_08task.cpp
+++ 03_1_08task.cpp
@@ -3,345 +3,179 @@
 #include <string.h>
 #include "os_file.h"
 
-#define mSwp(T, a, b) \
-    {                 \
-        T tmp = a;    \
-        a = b;        \
-        b = tmp;      \
-    }
-
 typedef unsigned short int _ushrtint;
 typedef unsigned int _uint;
 
 typedef struct iNode
 {
     _ushrtint is_dir;
-    _uint objSz;
-    _uint chldCnt;
+    _uint size;
+    _uint child_count;
     char *name;
-    char *fPth;
-    iNode **chld;
-    iNode *prnt;
+    char *full_path;
+    iNode **child;
+    iNode *parent;
 } iNode;
 
 _uint __dsksz = 0, __ocpdsz = 0;
-iNode *__ind = NULL;
-char *__cwd = NULL;
+iNode *__ind = NULL, *__cwd = NULL;
 
-void setup_file_manager(file_manager_t *fm);
 int create(int disk_size);
 int create_dir(const char *path);
 int create_file(const char *path, int file_size);
-int change_dir(const char *path);
-int remove(const char *path, int recursive);
-int destroy();
-int copy(const char *path, const char *to_path);
-void get_cur_dir(char *dst);
+void setup_file_manager(file_manager_t *fm);
 
-int iNdMkObj(const char *path, _ushrtint o_type, _uint o_size);
-int iNdChDir(const char *path);
-int vldTkn(const char *tkn);
-int iNdCkExst(const char *o_name);
-int iNdRm(const char *path, int recursive);
-int rmNode(iNode *node, int rootflag);
-int chkDsk(iNode *iNd);
-int indCpy(const char *path, const char *to_path);
-int iNdDstr();
-int mkCpNode(const char *path, iNode *iNd);
+int check_disk(iNode *node);
+int init_tree(int disk_size);
+int check_token(const char *tkn);
+int realloc_tree(iNode *node);
+void make_node(iNode *main_node, _ushrtint is_dir, _uint size, char **name, char **full_path, iNode *parent);
+int remove_node(iNode *node);
+int make_obj(const char *path, _ushrtint mode, _uint size);
 
-int rmNode(iNode *node, int rootflag)
+iNode *get_node(const char *path)
 {
-    if (node == NULL)
-    {
-        return 0;
-    }
-    for (_uint i = 0; i < node->chldCnt; ++i)
-    {
-        if (__ind->chld != NULL && __ind->chld[i] != NULL && __ind->chld[i]->prnt != NULL && (__ind->chld[i]->is_dir == 1 || __ind->chld[i]->is_dir == 0))
-        {
-            rmNode(node->chld[i], 0);
-        }
-        node->chld[i] = NULL;
-    }
-    __ocpdsz -= node->objSz;
-    free(node->name);
-    free(node->fPth);
-    free(node->chld);
-    node->name = NULL;
-    node->fPth = NULL;
-    node->chld = NULL;
-    node->prnt = NULL;
-    node = NULL;
-    if (!rootflag)
-        free(node);
-    return 1;
-}
-
-int iNdRm(const char *path, int recursive)
-{
-    if (strcmp(path, "/") == 0)
-    {
-        return 0;
-    }
-    char *tkn, *str, *tfr, *chkpath = (char *)calloc(sizeof(char), 1), *fchkpath = (char *)calloc(sizeof(char), 1);
-    while (__ind->prnt != NULL)
-        __ind = __ind->prnt;
+    iNode *ret_node = NULL;
+    while (__ind->parent != NULL)
+        __ind = __ind->parent;
+    char *token, *loop_path, *to_free, 
+         *passed_path = (char *)calloc(sizeof(char), 1), 
+         *full_path = (char *)calloc(sizeof(char), 1);
     if (path[0] == '/')
     {
-        tfr = str = strdup(path);
-        fchkpath = (char *)realloc(fchkpath, sizeof(char) * strlen(path) + 1);
-        strcpy(fchkpath, str);
+        to_free = loop_path = strdup(path);
+        full_path = (char *)realloc(full_path, sizeof(char) * strlen(path) + 1);
+        strcpy(full_path, loop_path);
     }
     if (path[0] != '/')
     {
-        tfr = str = (char *)calloc(sizeof(char), strlen(__cwd) + strlen(path) + 1);
-        strcat(str, __cwd);
-        strcat(str, path);
-        fchkpath = (char *)realloc(fchkpath, sizeof(char) * (strlen(str) + 1));
-        strcpy(fchkpath, str);
+        to_free = loop_path = (char *)calloc(sizeof(char), (strlen(__cwd->full_path) + strlen(path) + 1));
+        strcat(loop_path, __cwd->full_path);
+        strcat(loop_path, path);
+        full_path = (char *)realloc(full_path, sizeof(char) * strlen(path) + 1);
+        strcpy(full_path, loop_path);
     }
-    if (path == NULL || str == NULL)
+    if (loop_path == NULL || path == NULL)
         return 0;
-    while ((tkn = strsep(&str, "/")) != NULL)
+
+    while ((token = strsep(&loop_path, "/")) != NULL)
     {
-        if (strcmp(tkn, ".") == 0)
+        if (!strcmp(token, ".") || !check_token(token))
+            continue;
+        if (!strcmp(token, ".."))
         {
+            if (__ind->parent != NULL)
+                __ind = __ind->parent;
             continue;
         }
-        if (strcmp(tkn, "..") == 0)
-        {
-            if (__ind->prnt != NULL)
-                __ind = __ind->prnt;
-            continue;
-        }
-        if (!vldTkn(tkn))
-        {
-            continue;
-        }
-        for (_uint i = 0; i < __ind->chldCnt; ++i)
-        {
-            if (__ind->chld != NULL && __ind->chld[i] != NULL && __ind->chld[i]->prnt != NULL && (__ind->chld[i]->is_dir == 1 || __ind->chld[i]->is_dir == 0) && strcmp(__ind->chld[i]->name, tkn) == 0)
-            {
-                chkpath = (char *)realloc(chkpath, sizeof(char) * (strlen(chkpath) + strlen(tkn) + strlen("/") + 1));
-                strcat(chkpath, "/");
-                strcat(chkpath, tkn);
-                __ind = __ind->chld[i];
-                break;
+        for(_uint i = 0; i < __ind->child_count; ++i){
+            if(!strcmp(__ind->child[i]->name, token)){
+                passed_path = (char *)realloc(passed_path, sizeof(char) * (strlen(passed_path) + strlen(token) + strlen("/") + 1));
+                strcat(passed_path, "/");
+                strcat(full_path, token);
+                __ind = __ind->child[i];
             }
         }
     }
-
-    if (strcmp(chkpath, fchkpath) != 0)
-    {
-        free(chkpath);
-        free(fchkpath);
-        free(tfr);
-        return 0;
+    if(!strcmp(full_path, passed_path)){
+        ret_node = __ind;
     }
-    iNode *nodeToDelete = __ind;
-    if (!recursive && nodeToDelete->is_dir && nodeToDelete->chldCnt > 0)
-    {
-        free(tfr);
-        free(chkpath);
-        free(fchkpath);
-        return 0;
-    }
-
-    while (__ind->prnt != NULL)
-        __ind = __ind->prnt;
-    int result = rmNode(nodeToDelete, 0);
-    if (result)
-    {
-        nodeToDelete = NULL;
-        free(__cwd);
-        __cwd = strdup("/");
-    }
-    free(chkpath);
-    free(fchkpath);
-    free(tfr);
-    return result;
+    free(to_free);
+    free(full_path);
+    free(passed_path);
+    return ret_node;
 }
 
-int iNdChDir(const char *path)
-{
-    char *tkn, *str, *tfr;
-    while (__ind->prnt != NULL)
-        __ind = __ind->prnt;
-    if (path[0] == '/')
-    {
-        tfr = str = strdup(path);
-    }
-    if (path[0] != '/')
-    {
-        tfr = str = (char *)calloc(sizeof(char), (strlen(__cwd) + strlen(path) + 1));
-        strcat(str, __cwd);
-        strcat(str, path);
-    }
-    if (path == NULL || str == NULL)
-    {
-        return 0;
-    }
-    while ((tkn = strsep(&str, "/")) != NULL)
-    {
-        if (strcmp(tkn, ".") == 0)
-        {
-            continue;
-        }
-        if (strcmp(tkn, "..") == 0)
-        {
-            if (__ind->prnt != NULL)
-                __ind = __ind->prnt;
-            continue;
-        }
-        if (!vldTkn(tkn))
-        {
-            continue;
-        }
-        if (!iNdCkExst(tkn))
-        {
-            free(tfr);
-            return 0;
-        }
-    }
-    free(__cwd);
-    __cwd = strdup(__ind->fPth);
-    free(tfr);
+int remove_node(iNode *node){
     return 1;
 }
-
-// o_type: 1 - folder, 0 - file
-int iNdMkObj(const char *path, _ushrtint o_type, _uint o_size)
+// mode: 1 - folder, 0 - file
+int make_obj(const char *path, _ushrtint mode, _uint size)
 {
-    int isCreated = 0;
+    if(get_node(path) != NULL) return 0;
+    int is_created = 0;
     iNode *newNode = NULL;
-    while (__ind->prnt != NULL)
-        __ind = __ind->prnt;
-    char *tkn, *newpth, *tfr;
+    while (__ind->parent != NULL)
+        __ind = __ind->parent;
+    char *token, *loop_path, *to_free;
     if (path[0] == '/')
     {
-        tfr = newpth = strdup(path);
+        to_free = loop_path = strdup(path);
     }
     if (path[0] != '/')
     {
-        tfr = newpth = (char *)calloc(sizeof(char), (strlen(__cwd) + strlen(path) + 1));
-        strcat(newpth, __cwd);
-        strcat(newpth, path);
+        to_free = loop_path = (char *)calloc(sizeof(char), (strlen(__cwd->full_path) + strlen(path) + 1));
+        strcat(loop_path, __cwd->full_path);
+        strcat(loop_path, path);
     }
-
-    if (newpth == NULL || path == NULL)
+    if (loop_path == NULL || path == NULL)
         return 0;
-    while ((tkn = strsep(&newpth, "/")) != NULL)
+    while ((token = strsep(&loop_path, "/")) != NULL)
     {
-        if (strcmp(tkn, ".") == 0)
-            continue;
-        if (strcmp(tkn, "..") == 0)
+        if (!strcmp(token, ".") || !check_token(token))
+        continue;
+        if (!strcmp(token, ".."))
         {
-            if (__ind->prnt != NULL)
-                __ind = __ind->prnt;
+            if (__ind->parent != NULL)
+                __ind = __ind->parent;
             continue;
         }
-        if (!vldTkn(tkn))
-            continue;
-        if (!iNdCkExst(tkn))
-        {
-            if (isCreated && newNode != NULL)
-            {
-                isCreated = 0;
-                rmNode(newNode, 0);
-                break;
-            }
-            if (!__ind->is_dir)
-            {
-                continue;
-            }
-            if (__ocpdsz + o_size > __dsksz || strlen(__ind->fPth) + strlen(tkn) + strlen("/") > 128)
-            {
-                isCreated = 0;
-                break;
-            }
-            isCreated = 1;
-            ++__ind->chldCnt;
-            if (__ind->chld == NULL)
-                __ind->chld = (iNode **)malloc(sizeof(iNode *));
-            else
-                __ind->chld = (iNode **)realloc(__ind->chld, sizeof(iNode *) * __ind->chldCnt);
-            newNode = (iNode *)malloc(sizeof(iNode));
-            newNode->chld = NULL;
-            newNode->chldCnt = 0;
-            newNode->is_dir = o_type;
-            newNode->name = strdup(tkn);
-            newNode->objSz = o_size;
-            newNode->prnt = __ind;
-            newNode->fPth = (char *)calloc(sizeof(char), (strlen(__ind->fPth) + strlen(tkn) + strlen("/") + 1));
-            strcat(newNode->fPth, __ind->fPth);
-            strcat(newNode->fPth, newNode->name);
-            strcat(newNode->fPth, "/");
-            __ind->chld[__ind->chldCnt - 1] = newNode;
-            __ocpdsz += o_size;
+        if(is_created && newNode != NULL){
+            is_created = 0;
+            remove_node(newNode);
+            break;
         }
+        is_created = 1;
+        newNode = (iNode*)malloc(sizeof(iNode));
+        char *new_node_name = strdup(token);
+        char *new_node_full_path = (char*)calloc(sizeof(char), strlen(__ind->full_path) + strlen(token) + strlen("/") + 1);
+        strcat(new_node_full_path, __ind->full_path);
+        strcat(new_node_full_path, token);
+        strcat(new_node_full_path, "/");
+        make_node(newNode, mode, size, &new_node_name, &new_node_full_path, __ind);        
+        ++__ind->child_count;
+        if(__ind->child == NULL){
+            __ind->child = (iNode**)malloc(sizeof(iNode*));
+        }
+        else{
+            __ind->child = (iNode **)realloc(__ind->child, sizeof(iNode *) * __ind->child_count);
+        } 
+        __ind->child[__ind->child_count] = newNode;
     }
-    free(newpth);
-    free(tfr);
-    return isCreated;
+    free(to_free);
+    return is_created;
 }
 
-int create(int disk_size)
+int realloc_tree(iNode *node){
+    return 1;
+}
+
+void make_node(iNode *main_node, _ushrtint is_dir, _uint size, char **name, char **full_path, iNode *parent)
 {
-    if (chkDsk(__ind))
-    {
-        return 0;
-    }
+    main_node->child = NULL;
+    main_node->parent = parent;
+    main_node->child_count = 0;
+    main_node->is_dir = is_dir;
+    main_node->size = size;
+    main_node->name = *name;
+    main_node->full_path = *full_path;
+}
+
+int init_tree(int disk_size)
+{
     if (disk_size <= 0)
-    {
         return 0;
-    }
     __ind = (iNode *)malloc(sizeof(iNode));
     if (__ind == NULL)
-    {
         exit(0);
-    }
-    __ind->chld = NULL;
-    __ind->chldCnt = 0;
-    __ind->fPth = strdup("/");
-    __ind->is_dir = 1;
-    __ind->name = strdup("/");
-    __ind->objSz = 0;
-    __ind->prnt = NULL;
-    __cwd = strdup("/");
-    __dsksz = disk_size;
-    __ocpdsz = 0;
+    char *full_path = strdup("/");
+    char *name = strdup("/");
+    make_node(__ind, 1, 0, &name, &full_path, NULL);
+    __cwd = __ind;
     return 1;
 }
 
-void setup_file_manager(file_manager_t *fm)
-{
-    fm->create = create;
-    fm->create_dir = create_dir;
-    fm->create_file = create_file;
-    fm->change_dir = change_dir;
-    fm->get_cur_dir = get_cur_dir;
-    fm->remove = remove;
-    // fm->copy = copy;
-    fm->destroy = destroy;
-}
-int chkDsk(iNode *iNd) { return iNd == NULL ? 0 : 1; }
-
-// 1 - exist : 0 - not
-int iNdCkExst(const char *o_name)
-{
-    for (_uint i = 0; i < __ind->chldCnt; ++i)
-    {
-        // if (__ind->chld != NULL && strcmp(__ind->chld[i]->name, o_name) == 0 /*&& __ind->chld[i]->is_dir*/)
-        if (__ind->chld != NULL && __ind->chld[i] != NULL && __ind->chld[i]->prnt != NULL && (__ind->chld[i]->is_dir == 1 || __ind->chld[i]->is_dir == 0) && strcmp(__ind->chld[i]->name, o_name) == 0)
-        {
-            __ind = __ind->chld[i];
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int vldTkn(const char *tkn)
+int check_token(const char *tkn)
 {
     if (strlen(tkn) <= 0 || strlen(tkn) > 32 || !strcmp(tkn, "") || tkn[0] == '\0' /*!strcmp(tkn, ".") || !strcmp(tkn, "..") || tkn[0] == '.' ||*/)
     {
@@ -358,44 +192,38 @@ int vldTkn(const char *tkn)
     return 1;
 }
 
-int iNdDstr()
+void setup_file_manager(file_manager_t *fm)
 {
-    iNdChDir("/");
-    rmNode(__ind, 1);
-    free(__cwd);
-    free(__ind);
-    __ind = NULL;
-    __cwd = NULL;
-    return 1;
+    fm->create = create;
+    fm->create_dir = create_dir;
+    fm->create_file = create_file;
+    // fm->change_dir = change_dir;
+    // fm->get_cur_dir = get_cur_dir;
+    // fm->remove = remove;
+    // fm->copy = copy;
+    // fm->destroy = destroy;
 }
 
-int create_dir(const char *path) { return chkDsk(__ind) == 0 ? 0 : iNdMkObj(path, 1, 0); }
-int create_file(const char *path, int file_size) { return chkDsk(__ind) == 0 ? 0 : iNdMkObj(path, 0, file_size); }
-int change_dir(const char *path) { return chkDsk(__ind) == 0 ? 0 : iNdChDir((char *)path); }
-int remove(const char *path, int recursive) { return chkDsk(__ind) == 0 ? 0 : iNdRm(path, recursive); }
-// int copy(const char *path, const char *to_path) { return chkDsk(__ind) == 0 ? 0 : indCpy(path, to_path); }
-int destroy() { return chkDsk(__ind) == 0 ? 0 : iNdDstr(); }
-
-void get_cur_dir(char *dst)
-{
-    // fprintf(stdout, "CUR_DIR: %s\n", __cwd);
-}
+int create(int disk_size) { return check_disk(__ind) == 0 ? init_tree(disk_size) : 0; }
+int create_dir(const char *path) { return check_disk(__ind) == 0 ? 0 : make_obj(path, 1, 0); }
+int create_file(const char *path, int file_size) { return check_disk(__ind) == 0 ? 0 : make_obj(path, 0, file_size); }
+int check_disk(iNode *node) { return node == NULL ? 0 : 1; }
 
 void printTree(iNode *node)
 {
     printf("is_dir: %hu\n", node->is_dir);
-    printf("objSz: %u\n", node->objSz);
-    printf("chldCnt: %u\n", node->chldCnt);
+    printf("objSz: %u\n", node->size);
+    printf("chldCnt: %u\n", node->child_count);
     printf("name: %s\n", node->name);
-    printf("fPth: %s\n\n", node->fPth);
+    printf("fPth: %s\n\n", node->full_path);
     // Print information about children, if any
-    if (node->chldCnt > 0 && node->chld != NULL)
+    if (node->child_count > 0 && node->child != NULL)
     {
         printf("Children:\n");
-        for (_uint i = 0; i < node->chldCnt; ++i)
+        for (_uint i = 0; i < node->child_count; ++i)
         {
             printf("Child %u:\n", i + 1);
-            printTree(node->chld[i]);
+            printTree(node->child[i]);
         }
     }
 }
